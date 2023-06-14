d0b1301bbbcd6381d98bb219696909f37bf3a126
kelvin2go <=>
2023-06-14 20:48:00 +0800

    1.crawler 2. add trend and mongo db 3.eslint

--- index.js
+++ index.js
@@ -3,21 +3,24 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+require('dotenv').config();
+
 app.use(compression());
 
 const { GoogleTrendRouter } = require('./lib/googleTrend');
 const { TTSRouter } = require('./lib/textToSpeech');
 
 app.get('/', (req, res) => {
-  res.send(
-    'This is kelvin rapid api!'
-  );
+    res.send(
+        'This is kelvin rapid api!'
+    );
 });
 
+app.use('/trend', GoogleTrendRouter);
 app.use('/gt', GoogleTrendRouter);
 app.use('/googletrend', GoogleTrendRouter);
 app.use('/tts', TTSRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
